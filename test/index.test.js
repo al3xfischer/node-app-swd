47c899dd70c5d1dd281f926a751d669a89068770
Alex <34625198+LeqxCodes@users.noreply.github.com>
2019-10-17 20:19:41 +0200

    init test

---
+++ test/index.test.js
@@ -0,0 +1,41 @@
+// var assert = require('assert');
+
+// describe('Array', () => {
+//   describe('#indexOf()', () => {
+//     it('should return -1 when the value is not present',async () => {
+//         const res = await agent.get('/').type('text/html');
+//       assert.equal([1, 2, 3].indexOf(4), -1);
+//     });
+//   });
+// });
+
+var boot = require("../bin/www").boot,
+  shutdown = require("../bin/www").shutdown,
+  port = require("../bin/www").port,
+  superagent = require("superagent"),
+  expect = require("expect.js");
+
+describe("server", () => {
+  before(() => {
+    boot();
+  });
+
+  describe("homepage", () => {
+    it("should respond to GET", done => {
+      superagent.get("http://localhost:" + port).end((err, res) => {
+        expect(res.status).to.equal(200);
+        done();
+      });
+    });
+    it("should render h1 title", done => {
+      superagent.get("http://localhost:" + port).end((err, res) => {
+        expect(res.text).contain("<h1>Express</h1>");
+        done();
+      });
+    });
+  });
+
+  after(() => {
+    shutdown();
+  });
+});
