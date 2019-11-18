42110447f10d0603bc1206512487fe70271c0686
Alex <34625198+LeqxCodes@users.noreply.github.com>
2019-11-18 18:32:08 +0100

    changes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'SW Deployment' });
+  res.render('index', { title: 'SW Deployment K8s' });
 });
 
 module.exports = router;
